c40220407b64c6aef6440c7f656120b9300d207a
M Kartik Rao <120295872+nitiankartik@users.noreply.github.com>
2024-08-09 21:07:06 +0530

    WorkoutBuddy
    MERNapp, uses React.js, Node.js, Express.js and MongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts')
+const cors = require('cors')
+const userRoutes = require('./routes/userRoutes')
+
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes);
+
+app.use('/api/user', userRoutes);
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+    console.log('connected to db & listening on port 4000')
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
